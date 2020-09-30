60a0626e78379ac3184d02578010a9d738f06dfe
unknown <madchick@gmail.com>
2020-09-30 13:39:31 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# WeTube
+node.js, javascript and express<br><br><br><br>
+
+
+
+npm init  
+(package.json)
+
+npm install<br><br><br><br>
+
+
+
+npm install express<br><br>
+
+npm install @babel/node  
+npm install @babel/preset-env  
+npm install @babel/core<br><br>
+
+npm install nodemon -D<br><br><br><br>
+
+
+
+nodemon --exec babel-node index.js --delay 2<br><br><br><br>
+
+
+
+npm install helmet : security  
+npm install morgan : logging  
+npm install body-parser : form parsing  
+npm install cookie-parser : cookie parsing<br><br>
+
---
+++ wetube/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import morgan from "morgan"
+import helmet from "helmet"
+import cookieParser from "cookie-parser"
+import bodyParser from "body-parser"
+import { localMiddleware } from "./middlewares"
+import routes from "./routes"
+import globalRouter from "./routers/golbalRouter"
+import userRouter from "./routers/userRouter"
+import videoRouter from "./routers/videoRouter"
+
+const app = express();
+
+app.use(helmet());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(morgan("dev"));
+
+app.set("view engine", "pug");
+
+app.use(localMiddleware);
+
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
+
+export default app;
+
+
+
